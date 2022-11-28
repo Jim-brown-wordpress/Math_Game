b6b739f35696c0da646642fd3dd514a7d767fee7
Jim-brown-wordpress <donose1122@gmail.com>
2022-11-28 11:34:10 -0500

    third

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,4 +1,4 @@
 import socketIOClient from 'socket.io-client';
 const PORT = 8080;
-const socket = socketIOClient(`http://192.168.106.44:${PORT}`);
+const socket = socketIOClient(`http://localhost:${PORT}`);
 export default socket;
